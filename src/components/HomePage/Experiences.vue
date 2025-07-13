9e97ed2db9e745eaa8a2a58a3bba8133d5fc707c
MohammadAmin Tabesh <amintabeshyt@gmail.com>
2025-07-13 13:50:54 +0330

    feat: adjusted the animation

--- src/components/HomePage/Experiences.vue
+++ src/components/HomePage/Experiences.vue
@@ -2,8 +2,8 @@
 import TechIcons from "../../constants/TechIcons";
 import XPYears from "./XPYears.vue";
 import TechsCarousel from "./TechsCarousel.vue";
-import { useIntersectionObserver } from '@vueuse/core'
-import { useMotion } from '@vueuse/motion'
+import { useIntersectionObserver } from "@vueuse/core";
+import { useMotion } from "@vueuse/motion";
 import { ref } from "vue";
 
 const techs = [
@@ -13,52 +13,56 @@ const techs = [
   { title: "Tailwind", icon: TechIcons.tailwind, bgColor: "#90a1b9" },
 ];
 
-const xpYearsRef = ref(null)
-const carouselContainerRef = ref(null) // Changed to only animate container
+const xpYearsRef = ref(null);
+const carouselContainerRef = ref(null);
 
-useIntersectionObserver(
+// Animation for XPYears - will only run once
+const { stop: stopXPObserver } = useIntersectionObserver(
   xpYearsRef,
   ([{ isIntersecting }]) => {
     if (isIntersecting) {
       useMotion(xpYearsRef, {
         initial: { opacity: 0, y: 50 },
-        visibleOnce: { 
-          opacity: 1, 
+        enter: {  // Changed from visibleOnce to enter
+          opacity: 1,
           y: 0,
           transition: {
-            type: 'spring',
+            type: "spring",
             stiffness: 50,
             damping: 15,
             delay: 200,
-          }
-        }
-      })
+          },
+        },
+      });
+      stopXPObserver(); // Stop observing after first trigger
     }
   }
-)
+);
 
-useIntersectionObserver(
+// Animation for Carousel - will only run once
+const { stop: stopCarouselObserver } = useIntersectionObserver(
   carouselContainerRef,
   ([{ isIntersecting }]) => {
     if (isIntersecting) {
       useMotion(carouselContainerRef, {
         initial: { opacity: 0 },
-        visibleOnce: { 
+        enter: {  // Changed from visibleOnce to enter
           opacity: 1,
           transition: {
             duration: 600,
-            delay: 400
-          }
-        }
-      })
+            delay: 400,
+          },
+        },
+      });
+      stopCarouselObserver(); // Stop observing after first trigger
     }
   }
-)
+);
 </script>
 
 <template>
   <div class="tw-flex tw-flex-col md:tw-flex-row md:tw-items-center">
-    <div 
+    <div
       ref="xpYearsRef"
       class="tw-flex tw-w-full tw-justify-center tw-items-center md:tw-mb-10"
     >
